54ab160e5ebf73eaa0b54bfdfef4c1b33bbfaa3e
marcantoinem <marcantoine_manningham@outlook.com>
2022-12-26 16:33:28 -0500

    Challenge completed

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,9 @@ struct State {
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
+    is_color_pipeline: bool,
     render_pipeline: wgpu::RenderPipeline,
+    second_render_pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -107,13 +109,63 @@ impl State {
             },
             multiview: None, // 5.
         });
+        let shader = device.create_shader_module(wgpu::include_wgsl!("color.wgsl"));
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Second Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+        let second_render_pipeline =
+            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main", // 1.
+                    buffers: &[],           // 2.
+                },
+                fragment: Some(wgpu::FragmentState {
+                    // 3.
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        // 4.
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw, // 2.
+                    cull_mode: Some(wgpu::Face::Back),
+                    // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLIP_CONTROL
+                    unclipped_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: None, // 1.
+                multisample: wgpu::MultisampleState {
+                    count: 1,                         // 2.
+                    mask: !0,                         // 3.
+                    alpha_to_coverage_enabled: false, // 4.
+                },
+                multiview: None, // 5.
+            });
+        let is_color_pipeline = false;
         Self {
             surface,
             device,
             queue,
             config,
             size,
+            is_color_pipeline,
             render_pipeline,
+            second_render_pipeline,
         }
     }
 
@@ -127,7 +179,21 @@ impl State {
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        false
+        match event {
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Released,
+                        virtual_keycode: Some(VirtualKeyCode::Space),
+                        ..
+                    },
+                ..
+            } => {
+                self.is_color_pipeline = !self.is_color_pipeline;
+                true
+            }
+            _ => false,
+        }
     }
 
     fn update(&mut self) {}
@@ -160,7 +226,10 @@ impl State {
             })],
             depth_stencil_attachment: None,
         });
-        render_pass.set_pipeline(&self.render_pipeline); // 2.
+        match self.is_color_pipeline {
+            true => render_pass.set_pipeline(&self.second_render_pipeline),
+            false => render_pass.set_pipeline(&self.render_pipeline),
+        } // 2.
         render_pass.draw(0..3, 0..1); // 3.
         drop(render_pass);
 
